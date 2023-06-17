6352d5c522cd64e6bc232eab5f6d3ed87d52e023
John Moon <moon.john.a@gmail.com>
2023-06-16 20:34:22 -0700

    Fixes to build - still need to add python 3.9 requirement

--- tests/test_QifImporter.py
+++ tests/test_QifImporter.py
@@ -2,7 +2,14 @@ from qifparse.parser import QifParser
 
 qif_example1='test1.qif'
 
+import os
+import glob
+# this discovers test dir if in path tree
+cpaths=[r for r,d,f in os.walk(os.path.abspath('.')) if 'test1.qif' in f]
+if len(cpaths)>0:
+	cpath=cpaths[0]
+
 def test_Basic():
-	with open(qif_example1,'r') as f:
+	with open(os.path.join(cpath,qif_example1),'r') as f:
 		qf = QifParser(dayfirst=False).parse(f)
 	assert qf
