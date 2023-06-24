0e14707ab8bf54c15d075809934996418416b750
John Moon <moon.john.a@gmail.com>
2023-06-23 20:06:05 -0700

    Updates to yaml_util - added inverted output organized by account, and code to remove redundant regexes

--- bean_jmw/yaml_util.py
+++ bean_jmw/yaml_util.py
@@ -1,4 +1,4 @@
-# sort util for yaml files 
+# sort/combine/simplify util for yaml account assignment files 
 
 import yaml
 import argparse
@@ -11,9 +11,11 @@ ap=argparse.ArgumentParser()
 ap.add_argument("--existing","-e",required=True,help='Existing yaml file for account',default='')
 ap.add_argument("--add","-a",required=False,help='More entries to add, usually from unassigned file',default='')
 ap.add_argument("--override","-o",required=False,help='Use these entries to override any unassigned in existing',default='')
-ap.add_argument("--column_width","-cw",required=False,help='Align second column to this value',default=6)
+ap.add_argument("--column_width","-cw",required=False,help='Align second column to this value',default=0)
 ap.add_argument("--overwrite","-ow",required=False,help='Quietly overwrite entries that are added, otherwise error if in existing',default=False,action='store_true')
-ap.add_argument("--similar","-sp",required=False,help='Check regex patterns for subsetting',default=False,action='store_true')
+ap.add_argument("--similar","-sp",required=False,help='Check regex patterns for subsetting and remove redundant rules',default=False,action='store_true')
+
+ap.add_argument("--invert","-iv",required=False,help='Organize by account, not regex',default=False,action='store_true')
 
 clargs=ap.parse_args(sys.argv[1:])
 
@@ -75,9 +77,51 @@ else:
 	col_width=40
 	quotes="\""
 
-if not clargs.similar:	
-	# output combined, sorted items, preserving comments
-	for k,v in sorted_entries.items():
+# override value if set on command line
+if clargs.column_width!=0:
+	col_width=int(clargs.col_width)
+
+# find out if any regexes are a subset of one another...
+# if the simpler regex pattern assigns to the same account
+# using a more complex pattern, then remove the more complex pattern
+simplified_entries=OrderedDict(sorted_entries)
+if clargs.similar:
+	remove_pattern={}
+	similar_patterns={}
+	removed=0
+	for p1 in sorted_entries:
+		similar_patterns[p1]=[]
+		for p2 in sorted_entries:
+			if p1!=p2 and re.search(p1,p2):
+				similar_patterns[p1].append(p2)
+				# mark entries that are assigning to exact same account
+				if sorted_entries[p1]==sorted_entries[p2]:	
+					remove_pattern[p2]=True
+					removed += 1
+	simplified_entries=OrderedDict()
+	for e in sorted_entries:
+		if not e in remove_pattern:
+			simplified_entries[e]=sorted_entries[e]
+	print("# yaml_util: Removed {0} of {1} redundant regex patterns".format(removed,len(sorted_entries)))
+
+if clargs.invert:
+	# print by account
+	by_account={}
+	for k,v in simplified_entries.items():
+		if v in by_account:
+			by_account[v].append(k)
+		else:
+			by_account[v]=[k]
+
+	sorted_by_account=OrderedDict(sorted(by_account.items()))
+	for k,v in sorted_by_account.items():
+		print(k+":")
+		for vi in v:
+			print(" - "+"\""+vi+"\"")
+
+else:
+	# output combined, sorted, possibly simplified items, preserving comments
+	for k,v in simplified_entries.items():
 		nspace=max(col_width-len(str(k)),1)
 		comment=""
 		if k in comments_dict and v=="UNASSIGNED":
@@ -85,15 +129,3 @@ if not clargs.similar:
 		s_out = quotes + str(k) + quotes + ":" + " "*nspace + v + comment
 		print(s_out)
 
-# find out if any regexes are a subset of one another...
-if clargs.similar:
-	similar_patterns={}
-	for p1 in sorted_entries:
-		similar_patterns[p1]=[]
-		for p2 in sorted_entries:
-			if p1!=p2 and re.search(p1,p2):
-				similar_patterns[p1].append(p2)
-	
-	for p1 in similar_patterns:
-		if len(similar_patterns[p1])>0:
-			print(p1,similar_patterns[p1])
